1c4c1cebbfe4a417e477c0bd948a28d1a0d8a2c0
Angshu101 <angshu101.9733@gmail.com>
2022-07-03 11:58:46 +0530

    commit

--- smartpx_proj/smartpx_proj/settings.py
+++ smartpx_proj/smartpx_proj/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'smartpx_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartpx',
+        'USER':'smart_px',
+        'PASSWORD':'manishangshu',
+        'HOST':'smartpx.cfoyt8ldwtuy.us-east-2.rds.amazonaws.com'
     }
 }
 
