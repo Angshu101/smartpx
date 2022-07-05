72f7d844c5bb9b56ae6024d9cd7445a6afd1c076
Angshu101 <angshu101.9733@gmail.com>
2022-07-06 00:22:16 +0530

    commit

--- smartpx_proj/webpages/views.py
+++ smartpx_proj/webpages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'webpages/home.html')
+
+def product(request):
+    return render(request,'webpages/product.html')
+
+def about(request):
+    return render(request,'webpages/about.html')
+
+def contact(request):
+    return render(request,'webpages/contact.html')
