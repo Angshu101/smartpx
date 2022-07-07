74c754ba908fd02738a846f886c4aeb12eb4c1aa
Angshu101 <angshu101.9733@gmail.com>
2022-07-08 03:05:03 +0530

    commit

--- smartpx_proj/webpages/views.py
+++ smartpx_proj/webpages/views.py
@@ -5,7 +5,12 @@ def home(request):
     return render(request,'webpages/home.html')
 
 def product(request):
-    return render(request,'webpages/product.html')
+    product=Product.objects.all()
+    data={
+        
+        'product':product
+    }
+    return render(request,'webpages/product.html',data)
 
 def about(request):
     return render(request,'webpages/about.html')
