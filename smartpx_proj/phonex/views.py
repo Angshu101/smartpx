2e40f48f69c18684b56cabbab61562c091b3a67d
Angshu101 <angshu101.9733@gmail.com>
2022-07-09 22:12:08 +0530

    commit

--- smartpx_proj/phonex/views.py
+++ smartpx_proj/phonex/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from webpages.models import Product
 # Create your views here.
 def phonex(request):
     pass
 def search(request):
-   pass
+    searc_ph=Product.objects.all()
+    
+    if 'keyword' in request.GET:
+        keyword=request.GET['keyword']
+        if keyword:
+            searc_ph=searc_ph.filter(prod_name__icontains=keyword)
+    data={
+        'searc_ph':searc_ph,
+    }
+    return render(request,'phonex/search.html',data)
